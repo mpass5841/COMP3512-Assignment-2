467d5af91b1b4327a32cce4fb262bce35ed679bc
swimm830 <60192429+swimm830@users.noreply.github.com>
2021-04-07 16:35:59 -0600

    Add files via upload
    updated style and api-companies

--- pages/helpers/api-companies.php
+++ pages/helpers/api-companies.php
@@ -1,4 +1,6 @@
 <?php
+
+function getCompany(){
 require_once 'a2-helper.php';
 require_once 'configa2.inc.php';
 $conn = DatabaseHelper::createConnection(array(DBCONNSTRING,DBUSER, DBPASS));
@@ -6,11 +8,12 @@ $companies = new CompanyDB($conn);
 if(isset($_GET['symbol'])){
 $symbol = $_GET['symbol'];
     $companyList = $companies->getSingleCompany($symbol);
-    header('Content-Type: application/json');
-    echo json_encode($companyList, JSON_NUMERIC_CHECK+JSON_PRETTY_PRINT );  
+    //header('Content-Type: application/json');
+    return $companyList;  
 }else{
     $companyList = $companies->getAllCompanies();
-    header('Content-Type: application/json');
-    echo json_encode($companyList, JSON_NUMERIC_CHECK+JSON_PRETTY_PRINT );
+    //header('Content-Type: application/json');
+    return $companyList;
+}
 }
 ?>
