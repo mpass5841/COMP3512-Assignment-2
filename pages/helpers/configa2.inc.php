e61b57bd3a0caa94e4546e52825d1b2ad05b0968
eagga857 <60192527+eagga857@users.noreply.github.com>
2021-04-14 06:54:36 -0600

    Add files via upload

--- pages/helpers/configa2.inc.php
+++ pages/helpers/configa2.inc.php
@@ -1,8 +1,8 @@
 <?php
 //Makes a connection to the database
-define('DBHOST', 'localhost');
-define('DBNAME', 'a2'); //We named our database a2
-define('DBUSER', 'root');
-define('DBPASS', '');
+define('DBHOST', 'sql3.freemysqlhosting.net');
+define('DBNAME', 'sql3405599'); //We named our database a2
+define('DBUSER', 'sql3405599');
+define('DBPASS', 'BiCJU4FMcQ');
 define('DBCONNSTRING',"mysql:host=" . DBHOST . ";dbname=" . DBNAME . ";charset=utf8mb4;");
 ?>
